87a8d7502c3517ea8f306fe4dad932ed99846764
imnakez <imnakez@yandex.ru>
2017-01-30 18:39:40 +0300

    Теперь немного по другому проверяет браузер.

--- com_surveyforce/site/helpers/html.php
+++ com_surveyforce/site/helpers/html.php
@@ -867,19 +867,18 @@ class SurveyforceHtml {
 
             function sf_MakeRequest(url) {
                 var http_request = false;
-                if (window.XMLHttpRequest) { // Mozilla, Safari,...
+                try { // IE
+                    http_request = new ActiveXObject("Msxml2.XMLHTTP");
+                } catch (e) {
+                    try {
+                        http_request = new ActiveXObject("Microsoft.XMLHTTP");
+                    } catch (e) {
+                    }
+                }
+                if (window.XMLHttpRequest && !http_request) { // Mozilla, Safari,...
                     http_request = new XMLHttpRequest();
                     if (http_request.overrideMimeType) {
                         http_request.overrideMimeType('text/xml');
-                    }
-                } else if (window.ActiveXObject) { // IE
-                    try {
-                        http_request = new ActiveXObject("Msxml2.XMLHTTP");
-                    } catch (e) {
-                        try {
-                            http_request = new ActiveXObject("Microsoft.XMLHTTP");
-                        } catch (e) {
-                        }
                     }
                 }
                 if (!http_request) {
--- com_surveyforce/site/helpers/html/html.php
+++ com_surveyforce/site/helpers/html/html.php
@@ -869,21 +869,20 @@ class SurveyforceHtml {
 
             function sf_MakeRequest(url) {
                 var http_request = false;
-                if (window.XMLHttpRequest) { // Mozilla, Safari,...
-                    http_request = new XMLHttpRequest();
-                    if (http_request.overrideMimeType) {
-                        http_request.overrideMimeType('text/xml');
-                    }
-                } else if (window.ActiveXObject) { // IE
-                    try {
-                        http_request = new ActiveXObject("Msxml2.XMLHTTP");
-                    } catch (e) {
-                        try {
-                            http_request = new ActiveXObject("Microsoft.XMLHTTP");
-                        } catch (e) {
-                        }
-                    }
-                }
+                try { // IE
+	                http_request = new ActiveXObject("Msxml2.XMLHTTP");
+	            } catch (e) {
+	                try {
+	                    http_request = new ActiveXObject("Microsoft.XMLHTTP");
+	                } catch (e) {
+	                }
+	            }
+	            if (window.XMLHttpRequest && !http_request) { // Mozilla, Safari,...
+	                http_request = new XMLHttpRequest();
+	                if (http_request.overrideMimeType) {
+	                    http_request.overrideMimeType('text/xml');
+	                }
+	            }
                 if (!http_request) {
                     return false;
                 }
