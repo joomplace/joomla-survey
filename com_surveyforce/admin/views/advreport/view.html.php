f14c866053a04f2308177e5e6cb50c81f64c89a1
Vitaly Busel <v.busel@belitsoft.com>
2017-10-17 11:21:29 +0300

    Replacing deprecated function preg_replace to preg_replace_callback

--- com_surveyforce/admin/views/advreport/view.html.php
+++ com_surveyforce/admin/views/advreport/view.html.php
@@ -1452,7 +1452,8 @@ class SurveyforceViewAdvreport extends JViewLegacy {
 		$field_text = str_replace( "\n", ' ', $field_text );
 		$field_text = str_replace( "\r", ' ', $field_text );
 		$field_text = strtr( $field_text, array_flip($this->get_html_translation_table_my( ) ) );
-		$field_text = preg_replace( "/&#([0-9]+);/me", "chr('\\1')", $field_text );
+		//$field_text = preg_replace( "/&#([0-9]+);/me", "chr('\\1')", $field_text );
+        $field_text = preg_replace_callback("/&#([0-9]+);/m", function($matches){ return chr($matches[1])}, $field_text);
 		$field_text = '"'.$field_text.'"';
 		return $field_text;
 	}
@@ -1466,7 +1467,8 @@ class SurveyforceViewAdvreport extends JViewLegacy {
 		$field_text = str_replace( "\n", ' ', $field_text );
 		$field_text = str_replace( "\r", ' ', $field_text );
 		$field_text = strtr( $field_text, array_flip($this->get_html_translation_table_my( ) ) );
-		$field_text = preg_replace( "/&#([0-9]+);/me", "chr('\\1')", $field_text );
-		return $field_text;
+		//$field_text = preg_replace( "/&#([0-9]+);/me", "chr('\\1')", $field_text );
+        $field_text = preg_replace_callback("/&#([0-9]+);/m", function($matches){ return chr($matches[1])}, $field_text);
+        return $field_text;
 	}
 }
