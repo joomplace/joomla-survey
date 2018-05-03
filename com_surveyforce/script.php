cba15f4c68c3ba479a39818d116c161e39bfc11c
Alexander Kaberdin <a.kaberdin@belitsoft.com>
2018-05-03 15:36:06 +0300

    fix sql update

--- com_surveyforce/script.php
+++ com_surveyforce/script.php
@@ -26,7 +26,7 @@ class com_surveyforceInstallerScript
 		if(file_exists(JPATH_ADMINISTRATOR .'/components/com_surveyforce/surveyforce.xml')){
 			$xml = JFactory::getXML(JPATH_ADMINISTRATOR .'/components/com_surveyforce/surveyforce.xml');
 			$this->version_from = $version = preg_split( '/(\s|\.)/', $xml->version );
-			if($version[1]<=3 && $version[2]<=2 && $version[3]<6 && $xml->version != "3.2.5.001"){
+			if($version[0]<=3 && $version[1]<=2 && $version[2]<6 && $xml->version != "3.2.5.001"){
 				$db = JFactory::getDbo();
                 $sql = "UPDATE `#__survey_force_user_answers` ua LEFT JOIN `#__survey_force_quests` q ON ua.`quest_id`=q.`id` SET ua.`answer`=(@temp:=ua.`answer`), ua.`answer`=ua.`ans_field`, ua.`ans_field`=@temp WHERE q.`sf_qtype`=9";
                 $db->setQuery($sql);
