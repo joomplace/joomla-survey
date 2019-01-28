16195d2d25fbcd49df72a31fa1c522639d084d31
Serguei Filitchonok <fsvpost@gmail.com>
2019-01-28 14:19:49 +0300

    fixed characters in utf-8

--- com_surveyforce/site/assets/libchart/classes/PieChart.php
+++ com_surveyforce/site/assets/libchart/classes/PieChart.php
@@ -4,7 +4,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
         /** Libchart - PHP chart library
         *       
-        * Copyright (C) 2005-2006 Jean-Marc Tr�meaux (jm.tremeaux at gmail.com)
+        * Copyright (C) 2005-2006 Jean-Marc Trémeaux (jm.tremeaux at gmail.com)
         *       
         * This library is free software; you can redistribute it and/or
         * modify it under the terms of the GNU Lesser General Public
@@ -25,7 +25,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
         /**
         * Pie chart
         *
-        * @author   Jean-Marc Tr�meaux (jm.tremeaux at gmail.com)
+        * @author   Jean-Marc Trémeaux (jm.tremeaux at gmail.com)
         */
 
         class PieChart extends Chart
