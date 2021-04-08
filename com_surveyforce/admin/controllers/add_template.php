3a559c5de77a4888431b4c6d33ef2875e71bf53a
filitchonok_s <s.filitchonok@belitsoft.com>
2021-04-08 14:52:22 +0300

    Fixed installation of a new template through the admin panel: adding a new template to question plugins. Added instructions for self-assembly of a template's zip-file and an example of a zip-file for download. Minor styling improvements in the admin area.

--- com_surveyforce/admin/controllers/add_template.php
+++ com_surveyforce/admin/controllers/add_template.php
@@ -9,6 +9,12 @@
 
 defined('_JEXEC') or die('Restricted access');
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Filesystem\File;
+use Joomla\Archive\Archive;
+
 class SurveyforceControllerAdd_template extends JControllerForm
 {
     public function cancel($key = null)
@@ -18,14 +24,12 @@ class SurveyforceControllerAdd_template extends JControllerForm
 
     public function add()
     {
-        $file =  \JFactory::getApplication()->input->files->get('jform', array(), 'raw');
+        $this->checkToken();
+        $app  = Factory::getApplication();
+        $file = $app->input->files->get('jform', array(), 'raw');
 
-        if ( $file['package_file']['type'] == 'application/zip' || $file['package_file']['type'] == 'application/x-zip-compressed') {
-            jimport('joomla.filesystem.file');
-            jimport('joomla.filesystem.folder');
-            jimport('joomla.filesystem.archive');
+        if ($file['package_file']['type'] == 'application/zip' || $file['package_file']['type'] == 'application/x-zip-compressed') {
 
-            // Changing PHP settings.
             if ((int) ini_get('memory_limit') < 128) {
                 ini_set("memory_limit", "128M");
             }
@@ -34,49 +38,69 @@ class SurveyforceControllerAdd_template extends JControllerForm
                 set_time_limit(0);
             }
 
-            $package_name = JFile::stripExt($file['package_file']['name']);
-            $package_TempPath = JFactory::getConfig()->get("tmp_path").'/';
+            $package_name = File::stripExt($file['package_file']['name']);
+            $package_TempPath = Factory::getConfig()->get('tmp_path', JPATH_ROOT.'/tmp');
 
-            if (JFile::move($file['package_file']['tmp_name'], $package_TempPath.$package_name.'.zip')) {
+            if (File::move($file['package_file']['tmp_name'], $package_TempPath.'/'.$package_name.'.zip')) {
 
-                @JFolder::create($package_TempPath.$package_name.'/', 0755);
-                $package = JArchive::extract($package_TempPath.$package_name.'.zip', $package_TempPath.$package_name.'/');
+                @Folder::create($package_TempPath.'/'.$package_name.'/', 0755);
+                $archive = new Archive(array('tmp_path' => $package_TempPath));
+                $package = $archive->extract($package_TempPath.'/'.$package_name.'.zip', $package_TempPath.'/'.$package_name.'/');
 
                 if ($package) {
-                    JFile::delete($package_TempPath.$package_name.'.zip');
-                    $folder = '';
-                    if(!JFile::exists($package_TempPath.$package_name.'/template.php')) {
-                        //search template.php
-                        $folders = JFolder::folders($package_TempPath.$package_name);
-                        foreach($folders as $f) {
-                            if(JFile::exists($package_TempPath.$package_name.'/'.$f.'/template.php')) {
-                                $folder = '/'.$f;
+                    File::delete($package_TempPath.'/'.$package_name.'.zip');
+                    $folder_component = '';
+                    if(!File::exists($package_TempPath.'/'.$package_name.'/template.php')) {
+                        $folders = Folder::folders($package_TempPath.'/'.$package_name);
+                        foreach($folders as $folder) {
+                            if(File::exists($package_TempPath.'/'.$package_name.'/'.$folder.'/template.php')) {
+                                $folder_component = $folder;
                                 break;
                             }
                         }
-                        if($folder=='') {//JText::_('COM_SURVEYFORCE_ERROR_UPLOADING_TEMPLATE')
-                            \JFactory::getApplication()->enqueueMessage(JText::_('COM_SURVEYFORCE_ERROR_UPLOADING_TEMPLATE'), 'error');
-                            JFolder::delete($package_TempPath.$package_name);
+                        if($folder_component == '') {
+                            $app->enqueueMessage(Text::_('COM_SURVEYFORCE_ERROR_UPLOADING_TEMPLATE'), 'error');
+                            Folder::delete($package_TempPath.'/'.$package_name);
                             $this->setRedirect('index.php?option=com_surveyforce&view=templates');
                             return true;
                         }
                     }
 
+                    Folder::move($package_TempPath.'/'.$package_name.'/'.$folder_component, JPATH_COMPONENT_SITE.'/templates/'.$package_name);
 
-                    JFolder::move($package_TempPath.$package_name.$folder, JPATH_COMPONENT_SITE.'/templates/'.$package_name);
-                    if(JFolder::exists($package_TempPath.$package_name)) {
-                        JFolder::delete($package_TempPath.$package_name);
+                    $plugins_names = Folder::folders(JPATH_SITE.'/plugins/survey');
+                    foreach($plugins_names as $plugin_name) {
+                        if($plugin_name == 'pagebreak') {
+                            continue;
+                        }
+
+                        if(Folder::exists($package_TempPath.'/'.$package_name.'/plugins/'.$plugin_name)) {
+                            Folder::copy($package_TempPath.'/'.$package_name.'/plugins/'.$plugin_name,
+                                JPATH_SITE.'/plugins/survey/'.$plugin_name.'/tmpl/'.$package_name);
+                        } else {
+                            Folder::copy(JPATH_SITE.'/plugins/survey/'.$plugin_name.'/tmpl/surveyforce_standart',
+                                JPATH_SITE.'/plugins/survey/'.$plugin_name.'/tmpl/'.$package_name);
+                        }
                     }
 
+                    if(Folder::exists($package_TempPath.'/'.$package_name)) {
+                        Folder::delete($package_TempPath.'/'.$package_name);
+                    }
 
-                    $db = JFactory::getDBO();
-                    $db->setQuery("DELETE FROM #__survey_force_templates WHERE sf_name = '".$db->escape($package_name)."'");
-                    $db->execute();
+                    $db = Factory::getDbo();
+                    $query = $db->getQuery(true);
+                    $conditions = array(
+                        $db->qn('sf_name').'='.$db->q($db->escape($package_name))
+                    );
+                    $query->delete($db->qn('#__survey_force_templates'))
+                        ->where($conditions);
+                    $db->setQuery($query)
+                        ->execute();
 
                     $package_name_arr = explode('_', $package_name);
                     $sf_display_names = array();
-                    foreach ($package_name_arr as $item){
-                        if($item == mb_strtolower('surveyforce', 'UTF-8')){
+                    foreach ($package_name_arr as $item) {
+                        if($item == mb_strtolower('surveyforce', 'UTF-8')) {
                             continue;
                         }
                         $sf_display_names[] = $item;
@@ -85,13 +109,17 @@ class SurveyforceControllerAdd_template extends JControllerForm
                     $sf_display_names[] = 'template';
                     $sf_display_name = implode(' ', $sf_display_names);
 
-                    $db->setQuery("INSERT INTO #__survey_force_templates (`sf_name`, `sf_display_name`) VALUES ('" .$db->escape($package_name)."', '".$db->escape($sf_display_name)."')");
-                    $db->execute();
-                    \JFactory::getApplication()->enqueueMessage(JText::_('COM_SURVEYFORCE_SUCСESS_UPLOADING_TEMPLATE'));
+                    $sf_templates = new stdClass();
+                    $sf_templates->sf_name = $db->escape($package_name);
+                    $sf_templates->sf_display_name = $db->escape($sf_display_name);
+                    $db->insertObject('#__survey_force_templates', $sf_templates);
+
+                    $app->enqueueMessage(Text::_('COM_SURVEYFORCE_SUCСESS_UPLOADING_TEMPLATE'));
                 }
             }
         }
 
         $this->setRedirect('index.php?option=com_surveyforce&view=templates');
+        return true;
     }
 }
