59beb2415a755faf50ad0f62fd66daa069b9c298
filitchonok_s <s.filitchonok@belitsoft.com>
2022-04-28 20:29:24 +0400

    correction of typos

--- com_surveyforce/site/assets/libchart/classes/LineChart.php
+++ com_surveyforce/site/assets/libchart/classes/LineChart.php
@@ -5,7 +5,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
         /** Libchart - PHP chart library
         *       
-        * Copyright (C) 2005-2006 Jean-Marc Tr�meaux (jm.tremeaux at gmail.com)
+        * Copyright (C) 2005-2006 Jean-Marc Trémeaux (jm.tremeaux at gmail.com)
         *       
         * This library is free software; you can redistribute it and/or
         * modify it under the terms of the GNU Lesser General Public
@@ -26,7 +26,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
         /**
         * Line chart
         *
-        * @author   Jean-Marc Tr�meaux (jm.tremeaux at gmail.com)
+        * @author   Jean-Marc Trémeaux (jm.tremeaux at gmail.com)
         */
 
         class LineChart extends BarChart
